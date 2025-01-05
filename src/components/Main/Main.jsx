efac90029b328e1d7b9b2c8007b26e5c1fe73b44
Bilal Ahmad <you@example.com>
2025-01-05 12:52:23 +0500

    remving uncessory imports

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import Navbar from "../NavBar/Navbar";
 import Hero from "../Hero/Hero";
-import MainImage from "../images/mainBackground.png";
+
 import style from "./Main.module.css";
 import classNames from "classnames";
 import Dashboard from "../Dashboard/Dashboard";
-import Organizations from "../Organizations/Organizations";
 
 const Main = () => {
   return (
